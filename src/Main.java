5c741b46a2edb68a6142789d6ce1e5a6bd77b3ae
Bobylly <Bobylly@gmail.com>
2019-06-14 15:34:54 +0300

    - fix quantity loop, checked everything, clean.
    todo: fix stock name so it can't receive integers. // !urgent

--- src/Main.java
+++ src/Main.java
@@ -29,7 +29,6 @@ public class Main {
 
 
 
-
         // begin - stock name
         String stockName;
         System.out.println("add a stock name: (stockName) ");
@@ -43,7 +42,7 @@ public class Main {
             System.out.println("invalid stock name, please enter a valid stock name.");
             return;
         } // end - stock name todo: doesn't catch anything ... needs a revisit.
-        // todo: need to catch Integers, since stockName has to be String only
+        // todo: need to catch Integers, since stockName has to be String only; look into regex
 
 
         // begin - stock price
@@ -61,24 +60,15 @@ public class Main {
             if (!(stockPrice > 0)) {
                 System.out.println("invalid input. stock price has to be a positive number. try again.");
             }
-
         }
-
-//        if (!(stockPrice > 0.00000001)) {
-//            System.out.println("blbl invalid input. add a valid price for " + stockName + ".");
-//            return;
-//        }
-         // todo: add a loop, asking to try again (try again or cancel).
         // end - stock price
-
-
 
 
         // begin - stock amount (quantity)
         double quantity = 0;
         System.out.println("how many shares did you bought ? (quantity) ");
 
-        while (quantity <= 0) {
+        while (true) { // merge orice atat timp cat am o bucla infinita, spre ex for(;;)
             Scanner scannedQuantity = new Scanner(System.in);
             try {
                 quantity = scannedQuantity.nextDouble();
@@ -89,93 +79,18 @@ public class Main {
 
             if (quantity <= 0) {
                 System.out.println("invalid input. add a positive quantity. try again.");
+                continue;
             }
 
-            if (!(quantity > balance)) {
-                System.out.println("quantity is not bigger than balance");
-            } continue;
-//            double totalExpenses = stockPrice * quantity;
-//            if (totalExpenses > balance) {
-//                System.out.println("quantity can't exceed balance");
-//            }
-//            balance-=totalExpenses;
-//            System.out.println("congratulations. you bough " + quantity + " " + stockName + "shares " + " and" +
-//                    " it cost you " + totalExpenses + " usd. " + "your balance left is " + balance + " usd. the end.");
-
-        }
-
-        //todo: totalExpenses can't exceed balance.
-
-
-
-//            while (totalExpenses > balance) {
-//
-//                try {
-//                    quantity = scannedQuantity.nextDouble();
-//                    System.out.println("in try, before catch");
-//
-//
-//                } catch (Exception e){
-//                    System.out.println("in catch");
-//                    System.out.println("insufficient balance, buy less shares. ");
-//                } continue;
-//
-//            }
-
-
-
-
-   }
-
-
-
-
-
-
-
-    /* ----> the following methods will replace the actual current  way of doing things (everything in Main)
-             Initially I started with these methods, but for easier approach, i've stuck with plain Main method.
-             Will refactor (into classes, interfaces, methods, etc) after finishing the current approach (everything in Main).
-
-
-     */
-
-    private void balance() {     // balance -- starting balance
-
-        System.out.println("what's your USD balance ?");
-        Scanner keyboard = new Scanner(System.in);
-        double startingBalance = keyboard.nextDouble();
-        System.out.println("your balance is : " + startingBalance + ". Have fun gambling :)");
-    }
-
-    private void addStock() {    // addStock -- add a stock
-
-        System.out.println("would you like to buy a stock ? ");
-        Scanner keyboard = new Scanner(System.in);
-        String stockName = keyboard.nextLine();
-        System.out.println("you've bought " + stockName + " stock. Enjoy seeing it go to 0.");
-    }
-
-    private void stockPrice() {    // addStock -- add a stock
-
-        System.out.println("how much is one share worth these days ? ");
-        Scanner keyboard = new Scanner(System.in);
-        double stockPrice = keyboard.nextDouble();
-        System.out.println("you've bought the stock for " + stockPrice + " usd.");
-    }
-
-    private void stockAmount() {    // addStock -- add a stock
-
-        System.out.println("how many shares did you bought ? ");
-        Scanner keyboard = new Scanner(System.in);
-        double stockAmount = keyboard.nextDouble();
-        System.out.println("you've bought " + stockAmount + " nr shares.");
-    }
-
+            double totalExpenses = stockPrice * quantity;
+            if (totalExpenses > balance) {
+                System.out.println("quantity can't exceed balance");
+                continue;
+            }
+            balance-=totalExpenses;
+            System.out.println("congratulations. you bough " + quantity + " " + stockName + "shares " + " and" +
+                    " it cost you " + totalExpenses + " usd. " + "your balance left is " + balance + " usd. the end.");
+            break;
+        } // end stock amount (quantity)
+   } // end main
 }
-
-/*
-
-
-
- */
